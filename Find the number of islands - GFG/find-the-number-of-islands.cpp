5ac953f254c1808f867ff120ed55bd22d8346dd9
Ram Medam <92446670+ram9666@users.noreply.github.com>
2023-06-13 15:03:38 +0530

    Added solution - LeetHub

---
+++ Find the number of islands - GFG/find-the-number-of-islands.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find the number of islands.
+    int dx[8] = {0,1,1,1,0,-1,-1,-1};
+    int dy[8] = {1,1,0,-1,-1,-1,0,1};
+    void func(int x,int y,vector<vector<char>>& grid,vector<vector<int>>& vis)
+    {
+        vis[x][y]=1;
+        for(int i=0;i<8;i++)
+        {
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            if(nx>=0 and ny>=0 and nx<grid.size() and ny< grid[0].size() and grid[nx][ny]=='1' and vis[nx][ny]==0)
+            {
+                func(nx,ny,grid,vis);
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1' and vis[i][j]==0)
+                {
+                    cnt++;
+                    func(i,j,grid,vis);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<char>> grid(n, vector<char>(m, '#'));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        int ans = obj.numIslands(grid);
+        cout << ans << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
