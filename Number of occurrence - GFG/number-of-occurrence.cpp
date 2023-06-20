8569fe1649c58a8f97571a63b8de75310e688720
Ram Medam <92446670+ram9666@users.noreply.github.com>
2023-06-20 10:53:17 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -13,12 +13,49 @@ public:
 	
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int count =0;
-	    for(int i=0;i<n;i++)
+	    int i=0;
+	    int j=n-1;
+	    int low=-1;
+	    while(i<=j)
 	    {
-	        if(x==arr[i]) count++;
+	        int mid = (i+j)>>1;
+	        if(x==arr[mid])
+	        {
+	            low = mid;
+	            j = mid-1;
+	        }
+	        else if(x<arr[mid]){
+	            j = mid-1;
+	        }
+	        else
+	        {
+	            i = mid+1;
+	        }
 	    }
-	    return count;
+	    
+	     i=0;
+	     j=n-1;
+	    int high=-1;
+	    while(i<=j)
+	    {
+	        int mid = (i+j)>>1;
+	        if(x==arr[mid])
+	        {
+	            high = mid;
+	            i = mid+1;
+	        }
+	        else if(x>arr[mid]){
+	            i = mid+1;
+	        }
+	        else
+	        {
+	            j = mid-1;
+	        }
+	    }
+	    
+	    if(low ==-1) return 0;
+	    return high-low+1;
+	   
 	   
 	}
 };
