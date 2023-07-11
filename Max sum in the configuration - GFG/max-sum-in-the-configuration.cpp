099d12121dcd147ae0dbb55d7d0ed25bb0e38816
Ram Medam <92446670+ram9666@users.noreply.github.com>
2023-07-11 15:45:27 +0530

    Added solution - LeetHub

---
+++ Max sum in the configuration - GFG/max-sum-in-the-configuration.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int max_sum(int A[],int N);
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>A[i];
+        }
+
+        cout<<max_sum(A,N)<<endl;
+        /*keeping track of the total sum of the array*/
+
+    }
+}
+
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+int max_sum(int A[],int n)
+{
+        //Your code here
+      int sum=0;
+      int preSum=0;
+      for(int i=0;i<n;i++)
+      {
+          sum+=A[i];
+          preSum+= A[i]*i; 
+          
+      }
+      
+      int ans=preSum;
+      for(int i=1;i<n;i++)
+      {
+          preSum = preSum + sum- n*A[n-i];
+          ans = max(ans,preSum);
+      }
+      return ans;
+   
+          
+}
