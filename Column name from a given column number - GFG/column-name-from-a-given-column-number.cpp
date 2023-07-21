aed64873e93c6f4603daef900fd48373e94c3453
Rama Krishna Reddy Medam <92446670+ram9666@users.noreply.github.com>
2023-07-21 17:31:10 +0530

    Added solution - LeetHub

---
+++ Column name from a given column number - GFG/column-name-from-a-given-column-number.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    string colName (long long int n)
+    {
+        // your code here
+        string ans="";
+        while(n)
+        {
+            int k = (n-1)%26;
+            char res = (k)+'A';
+            ans = res+ans;
+            n = (n-1)/26;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		long long int n; cin >> n;
+		Solution ob;
+		cout << ob.colName (n) << '\n';
+	}
+}
+
+// } Driver Code Ends
