0d3238646300513c72078c8f252b748acea44df4
Rama Krishna Reddy Medam <92446670+ram9666@users.noreply.github.com>
2023-08-20 11:49:04 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,53 +10,11 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
-	
 	int count(int arr[], int n, int x) {
 	    // code here
-	    int i=0;
-	    int j=n-1;
-	    int low=-1;
-	    while(i<=j)
-	    {
-	        int mid = (i+j)>>1;
-	        if(x==arr[mid])
-	        {
-	            low = mid;
-	            j = mid-1;
-	        }
-	        else if(x<arr[mid]){
-	            j = mid-1;
-	        }
-	        else
-	        {
-	            i = mid+1;
-	        }
-	    }
-	    
-	     i=0;
-	     j=n-1;
-	    int high=-1;
-	    while(i<=j)
-	    {
-	        int mid = (i+j)>>1;
-	        if(x==arr[mid])
-	        {
-	            high = mid;
-	            i = mid+1;
-	        }
-	        else if(x>arr[mid]){
-	            i = mid+1;
-	        }
-	        else
-	        {
-	            j = mid-1;
-	        }
-	    }
-	    
-	    if(low ==-1) return 0;
-	    return high-low+1;
-	   
-	   
+	    int u = upper_bound(arr,arr+n,x)-arr;
+	    int l = lower_bound(arr,arr+n,x)-arr;
+	    return (u-l);
 	}
 };
 
